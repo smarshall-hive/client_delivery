23248eb8c9c7be7db5fd6d2cea7a07d51b57f433
smarshall-hive <sophie@thehive.ai>
2023-06-19 16:23:56 -0700

    new status split function :pepe-hyper:

---
+++ client_delivery_functions.py
@@ -0,0 +1,13 @@
+# function for splitting out moderator stauses: takes Hive export (my_table) and max possible moderator judgements (Maximum Worker Count) as parameters
+def extract_statuses(my_table, n_statuses):
+    import json
+    def parse_json(value):
+        try:
+            return json.loads(value)
+        except (json.JSONDecodeError, TypeError):
+            return None
+    my_table['json_loads'] = my_table['statuses'].apply(lambda x: parse_json(str(x)))
+    for i in range(n_statuses):
+        colname = 'mod' + str(i + 1)
+        my_table[colname] = my_table['json_loads'].str[0].str['statuses'].str[i].str['status']
+    return my_table
